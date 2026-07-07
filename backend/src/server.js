ff3d4c8027d3dd1e67d4e1de31b1bf2625f7c6ae
Nayab829 <nayabg829@gmail.com>
2026-07-07 19:18:53 +0500

    setup express backend with initial server configuration

---
+++ backend/src/server.js
@@ -0,0 +1,9 @@
+import app from "./app.js";
+import "dotenv/config"
+
+const port = process.env.PORT | 5000;
+
+app.listen(port, () => {
+    console.log(`App is running at http://localhost:${port}`);
+
+})
